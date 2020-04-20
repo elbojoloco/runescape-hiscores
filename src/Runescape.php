ff3da58e346dfdf57d30c06fe335e25e4b840698
Lucas Snel <lucas-snel@live.nl>
2020-04-20 23:09:07 +0200

    Added all documentation to Runescape.php

--- src/Runescape.php
+++ src/Runescape.php
@@ -4,31 +4,47 @@ namespace Elbojoloco\RunescapeHiscores;
 
 class Runescape {
     /**
+     * Retrieves the given player (RSN) from the RS3 hiscores.
+     *
      * @param  string  $rsn  The runescape name to look up.
      *
      * @return \Elbojoloco\RunescapeHiscores\Player
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RsnMissingException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\UnknownHiscoresTypeException
      */
-    static function rs3(string $rsn)
+    static function rs3(string $rsn): Player
     {
         return (new RunescapeClient())->rs3($rsn);
     }
 
     /**
+     * Retrieves the given player (RSN) from the Old School hiscores.
+     *
      * @param  string  $rsn  The runescape name to look up.
      *
      * @return \Elbojoloco\RunescapeHiscores\Player
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RsnMissingException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\UnknownHiscoresTypeException
      */
-    static function oldschool(string $rsn)
+    static function oldschool(string $rsn): Player
     {
         return (new RunescapeClient())->oldschool($rsn);
     }
 
     /**
+     * Retrieves the given player (RSN) from the given hiscore.
+     *
      * @param  string  $type  The hiscores to use. Accepted: "rs3" or "oldschool".
      * @param  string  $rsn  The runescape name to look up.
      *
      * @return \Elbojoloco\RunescapeHiscores\Player
      * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RsnMissingException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException
      * @throws \Elbojoloco\RunescapeHiscores\Exceptions\UnknownHiscoresTypeException
      */
     static function get(string $type, string $rsn): Player
