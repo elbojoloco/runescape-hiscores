f4b8723ecb97fe229ff095993dfe3f8e0881cd06
Lucas Snel <lucas-snel@live.nl>
2020-04-21 20:55:42 +0200

    Removed redundant public methods, removed local $rsn, refactored $type usage throughout RunescapeClient class, DRY'd up code.

--- src/RunescapeClient.php
+++ src/RunescapeClient.php
@@ -2,33 +2,32 @@
 
 namespace Elbojoloco\RunescapeHiscores;
 
-use Elbojoloco\RunescapeHiscores\Exceptions\RsnMissingException;
+use Elbojoloco\RunescapeHiscores\Exceptions\InvalidRsnException;
 use Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException;
 use Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException;
-use Elbojoloco\RunescapeHiscores\Exceptions\UnknownHiscoresTypeException;
+use Elbojoloco\RunescapeHiscores\Exceptions\InvalidHiscoreTypeException;
 use Zttp\Zttp;
 
 class RunescapeClient
 {
-    /**
-     * @var bool
-     */
-    private $oldschool = false;
+    const RS3_HISCORE = 'hiscore';
+    const OLDSCHOOL_HISCORE = 'hiscore_oldschool';
+    const TYPE_RS3 = 'rs3';
+    const TYPE_OLDSCHOOL = 'oldschool';
 
     /**
-     * @var bool
+     * The hiscores type to call. Either "rs3" or "oldschool"
+     *
+     * @var string $hiscoreType
      */
-    private $rs3 = false;
+    private $hiscoreType;
 
     /**
-     * @var string
-     */
-    private $rsn;
-
-    /**
+     * The complete list of OSRS skills in order
+     *
      * @var string[]
      */
-    private $osrsSkills = [
+    private static $osrsSkills = [
         0 => 'Overall',
         1 => 'Attack',
         2 => 'Defence',
@@ -56,9 +55,11 @@ class RunescapeClient
     ];
 
     /**
+     * The list of RS3 skills that should be merged into the OSRS skills for the RS3 hiscores
+     *
      * @var string[]
      */
-    private $rs3Skills = [
+    private static $rs3Skills = [
         4 => 'Constitution',
         24 => 'Summoning',
         25 => 'Dungeoneering',
@@ -67,74 +68,38 @@ class RunescapeClient
     ];
 
     /**
-     * Get a player's stats using the RS3 hiscores.
-     *
-     * @param  string  $rsn
-     *
-     * @return \Elbojoloco\RunescapeHiscores\Player
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RsnMissingException
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\UnknownHiscoresTypeException
-     */
-    public function rs3(string $rsn): Player
-    {
-        $this->rsn = $rsn;
-        $this->oldschool = false;
-        $this->rs3 = true;
-
-        return $this->get();
-    }
-
-    /**
-     * Get a player's stats using the Old School hiscores.
-     *
-     * @param  string  $rsn
-     *
-     * @return \Elbojoloco\RunescapeHiscores\Player
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RsnMissingException
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\UnknownHiscoresTypeException
-     */
-    public function oldschool(string $rsn): Player
-    {
-        $this->rsn = $rsn;
-        $this->oldschool = true;
-        $this->rs3 = false;
-
-        return $this->get();
-    }
-
-    /**
-     * Get the player's stats. Pass a type ("rs3" or "oldschool") and a runescape name when calling directly.
+     * Validate the inputs and call the API.
      *
      * @param  string  $type
      * @param  string  $rsn
      *
      * @return \Elbojoloco\RunescapeHiscores\Player
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RsnMissingException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\InvalidRsnException
      * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException
      * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException
-     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\UnknownHiscoresTypeException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\InvalidHiscoreTypeException
      */
-    public function get(string $type = '', string $rsn = ''): Player
+    public function hiscore(string $type, string $rsn): Player
     {
-        if ([$type, $rsn] = $this->formatTypeAndRsn($type, $rsn)) {
-            return $this->{$type}($rsn);
-        }
+        [$type, $rsn] = $this->formatTypeAndRsn($type, $rsn);
 
-        if (! $this->type()) {
-            throw new UnknownHiscoresTypeException(
-                'You may not call "get()" directly without specifying a Hiscore type of either "rs3" or "oldschool" and a valid RSN'
-            );
-        }
+        $this->hiscoreType = $type;
 
-        if (! $this->rsn) {
-            throw new RsnMissingException('RSN is required and must be at least 1 character long');
-        }
+        return $this->get($rsn);
+    }
 
-        $body = $this->sendRequest();
+    /**
+     * Call the API and return a Player instance that contains the RSN and the player's stats.
+     *
+     * @param  string  $rsn
+     *
+     * @return \Elbojoloco\RunescapeHiscores\Player
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException
+     */
+    private function get(string $rsn): Player
+    {
+        $body = $this->sendRequest($rsn);
 
         $skills = $this->skills();
         $stats = [];
@@ -145,26 +110,32 @@ class RunescapeClient
             $stats[$skills[$i]] = compact('rank', 'level', 'experience');
         }
 
-        return new Player($this->rsn, $stats);
+        return new Player($rsn, $stats);
     }
 
     /**
      * Sends the request to the hiscores endpoint and handles HTTP errors.
      *
+     * @param  string  $rsn
+     *
      * @return false|string[]
      * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeHiscoresFailedException
      * @throws \Elbojoloco\RunescapeHiscores\Exceptions\RunescapeNameNotFoundException
      */
-    private function sendRequest()
+    private function sendRequest(string $rsn)
     {
-        $response = Zttp::get($this->requestUrl());
+        $response = Zttp::get(
+            $this->requestUrl($this->hiscoreEndpoint(), $rsn)
+        );
 
-        if (($status = $response->status()) !== 200) {
-            if ($status === 404) {
-                throw new RunescapeNameNotFoundException("{$this->type()} hiscores lookup failed for RSN \"{$this->rsn}\"");
-            }
+        $status = $response->status();
 
-            throw new RunescapeHiscoresFailedException("{$this->type()} hiscores request failed with status code: {$status}");
+        if ($status === 404) {
+            throw new RunescapeNameNotFoundException("{$this->hiscoreType} hiscores lookup failed for RSN \"{$rsn}\"");
+        }
+
+        if ($status !== 200) {
+            throw new RunescapeHiscoresFailedException("{$this->hiscoreType} hiscores request failed with status code: {$status}");
         }
 
         return explode("\n", $response->body());
@@ -175,19 +146,27 @@ class RunescapeClient
      *
      * @return string
      */
-    private function hiscore(): string
+    private function hiscoreEndpoint(): string
     {
-        return $this->oldschool ? 'hiscore_oldschool' : 'hiscore';
+        $endpoints = [
+            self::TYPE_RS3 => self::RS3_HISCORE,
+            self::TYPE_OLDSCHOOL => self::OLDSCHOOL_HISCORE,
+        ];
+
+        return $endpoints[$this->hiscoreType];
     }
 
     /**
      * Get the full request URL with filled parameters.
      *
+     * @param  string  $endpoint
+     * @param  string  $rsn
+     *
      * @return string
      */
-    private function requestUrl(): string
+    private function requestUrl(string $endpoint, string $rsn): string
     {
-        return vsprintf('http://services.runescape.com/m=%s/index_lite.ws?player=%s', [$this->hiscore(), $this->rsn]);
+        return vsprintf('http://services.runescape.com/m=%s/index_lite.ws?player=%s', [$endpoint, $rsn]);
     }
 
     /**
@@ -197,25 +176,12 @@ class RunescapeClient
      */
     private function skills()
     {
-        return $this->oldschool ? $this->osrsSkills : array_replace($this->osrsSkills, $this->rs3Skills);
-    }
+        $skills = [
+            self::TYPE_OLDSCHOOL => self::$osrsSkills,
+            self::TYPE_RS3 => array_replace(self::$osrsSkills, self::$rs3Skills),
+        ];
 
-    /**
-     * Get the hiscore type. Returns false when no type has been set.
-     *
-     * @return bool|string
-     */
-    private function type()
-    {
-        if ($this->rs3) {
-            return 'rs3';
-        }
-
-        if ($this->oldschool) {
-            return 'oldschool';
-        }
-
-        return false;
+        return $skills[$this->hiscoreType];
     }
 
     /**
@@ -224,20 +190,26 @@ class RunescapeClient
      * @param  string  $type
      * @param  string  $rsn
      *
-     * @return array|bool
+     * @return array
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\InvalidHiscoreTypeException
+     * @throws \Elbojoloco\RunescapeHiscores\Exceptions\InvalidRsnException
      */
-    private function formatTypeAndRsn(string $type, string $rsn)
+    private function formatTypeAndRsn(string $type, string $rsn): array
     {
-        if (! $type || ! $rsn) {
-            return false;
+        if (! $type) {
+            throw new InvalidHiscoreTypeException("The hiscore type is required.");
+        }
+
+        if (! $rsn) {
+            throw new InvalidRsnException('The Runescape name is required.');
         }
 
         $type = strtolower(
             preg_replace('/\s/', '', $type)
         );
 
-        if (! in_array($type, ['rs3', 'oldschool'])) {
-            return false;
+        if (! in_array($type, [self::TYPE_RS3, self::TYPE_OLDSCHOOL])) {
+            throw new InvalidHiscoreTypeException("The given hiscore \"{$type}\" is invalid, must be either \"rs3\" or \"oldschool\"");
         }
 
         return [$type, $rsn];
